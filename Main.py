01a4e8c01aa959759f827691b41a915233e4d4e1
Sanjay Swain <sanjayswain450@gmail.com>
2020-07-26 15:15:39 +0530

    Added help command.

--- Main.py
+++ Main.py
@@ -10,16 +10,12 @@ conn = sqlite3.connect('C:/Contact Book/contacts.db')
 c = conn.cursor()
 c.execute('CREATE TABLE IF NOT EXISTS contact_info (Name char(25), Phone char(25), Email char(40))')
 
-command_list = {
-    'create': data_insertion,
-    'delete': delete_data,
-    'find': get_contact_details,
-}
-
+print("Welcome")
+print("Type help to know the commands")
 while True:
     Command = input("--> ").lower()
     if Command.split()[0] in command_list.keys():
-        command_list[Command.split()[0]](Command.split()[1])
+        command_list[Command]()
     elif Command == 'save' or Command == 'exit':
         break
     else:
--- Modules/MainModules.py
+++ Modules/MainModules.py
@@ -1,4 +1,5 @@
-def data_insertion(name, cursor):
+def data_insertion(cursor):
+    name = input('Enter the name: ')
     phone = input('Enter the phone number :')
     email = input('Enter the email ID :')
     cursor.execute(f"INSERT INTO contact_info VALUES('{name}', '{phone}', '{email}')")
@@ -9,14 +10,28 @@ def check_all_contacts(cursor):
         print(row)
 
 
-def get_contact_details(name_id, cursor):
+def get_contact_details(cursor):
+    name_id = input("Enter the name: ")
     cursor.execute(f"SELECT * FROM contact_info WHERE name = '{str(name_id)}'")
     print(cursor.fetchall())
     if name_id == 'all':
         check_all_contacts(cursor)
 
 
-def delete_data(data, cursor):
+def delete_data(cursor):
+    data = input('Enter the name: ')
     cursor.execute(f"DELETE FROM contact_info WHERE name = '{data}'")
     if data == 'all':
         cursor.execute(f"DROP TABLE contact_info")
+
+
+def help():
+    print(command_list.keys())
+
+
+command_list = {
+    'create': data_insertion,
+    'delete': delete_data,
+    'find': get_contact_details,
+    'help': help
+}
