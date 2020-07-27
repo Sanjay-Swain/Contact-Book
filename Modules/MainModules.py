ee9d401896cb13e40bedac7597950cf87abeb103
Sanjay Swain <sanjayswain450@gmail.com>
2020-07-27 15:19:19 +0530

    Code improvements and preparation for new features.

--- Modules/MainModules.py
+++ Modules/MainModules.py
@@ -7,25 +7,29 @@ def data_insertion(cursor):
 
 def check_all_contacts(cursor):
     for row in cursor.execute("SELECT * FROM contact_info "):
-        print(row)
+        if len(row) > 0:
+            print(row)
 
 
 def get_contact_details(cursor):
     name_id = input("Enter the name: ")
-    cursor.execute(f"SELECT * FROM contact_info WHERE name = '{str(name_id)}'")
-    print(cursor.fetchall())
     if name_id == 'all':
         check_all_contacts(cursor)
+    else:
+        cursor.execute(f"SELECT * FROM contact_info WHERE name = '{str(name_id)}'")
+        print(cursor.fetchall())
 
 
 def delete_data(cursor):
     data = input('Enter the name: ')
-    cursor.execute(f"DELETE FROM contact_info WHERE name = '{data}'")
     if data == 'all':
         cursor.execute(f"DROP TABLE contact_info")
+    else:
+        cursor.execute(f"DELETE FROM contact_info WHERE name = '{data}'")
 
 
 def help(cursor):
+    print(cursor)
     print(command_list.keys())
 
 
@@ -33,5 +37,5 @@ command_list = {
     'create': data_insertion,
     'delete': delete_data,
     'find': get_contact_details,
-    'help': help
+    'help': help,
 }
